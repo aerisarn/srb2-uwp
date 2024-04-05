9d3699689f5a458c2c8107b4ffa780f7801ceb80
e.canepa <e.canepa@isacsrl.it>
2024-04-05 18:01:52 +0200

    UWP appdata handling

--- src/d_main.c
+++ src/d_main.c
@@ -1324,7 +1324,7 @@ void D_SRB2Main(void)
 		else
 		{
 			// use user specific config file
-#ifdef DEFAULTDIR
+#if defined (DEFAULTDIR) && !defined(_WINDOWS_UWP)  
 			snprintf(srb2home, sizeof srb2home, "%s" PATHSEP DEFAULTDIR, userhome);
 			snprintf(downloaddir, sizeof downloaddir, "%s" PATHSEP "DOWNLOAD", srb2home);
 			if (dedicated)
@@ -1338,6 +1338,7 @@ void D_SRB2Main(void)
 
 			snprintf(luafiledir, sizeof luafiledir, "%s" PATHSEP "luafiles", srb2home);
 #else // DEFAULTDIR
+			//On uwp we want to load everything from a folder
 			snprintf(srb2home, sizeof srb2home, "%s", userhome);
 			snprintf(downloaddir, sizeof downloaddir, "%s", userhome);
 			if (dedicated)
@@ -1771,8 +1772,14 @@ void D_SRB2Main(void)
 	}
 }
 
+const char* uwp_GetAppDataPath();
+
 const char *D_Home(void)
 {
+#ifdef _WINDOWS_UWP
+	return uwp_GetAppDataPath();
+#endif
+
 	const char *userhome = NULL;
 
 #ifdef ANDROID
